6cf7b3cc0cbf745099bd241a8efc0c28a1ac7f6b
Jordan <jallen-chunn@appdev.yearup.org>
2024-10-21 15:39:42 -0500

    Added Exercise 1.B

---
+++ variable_examples.py
@@ -0,0 +1,14 @@
+customer_id = '1' #string ; not used in calculations
+first_name = 'Glub' #string; name
+middle_name = 'Tubbis' #string; name
+last_name = 'Wepple' #string; name
+gender = 'male' #string; not used in calculations
+date_of_birth = '04/29/1738' #string; not used in calculations
+drivers_liscence_id = "Y512-6122-7620" #string; not used in calculations
+auto_policy_number = " " #string; not used in calculations
+
+my_name = 'Jordan'
+my_state = 'Nevada'
+my_city = 'Las Vegas'
+
+#Lab 2
