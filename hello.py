6521125d1edbf166a4449e411de233f5ca554266
Jordan <jallen-chunn@appdev.yearup.org>
2024-10-21 14:19:41 -0500

    Added hello.py

---
+++ hello.py
@@ -0,0 +1,20 @@
+print('Hello world!')
+
+message = 'Hello world!'
+print(message)
+
+dollars = 3
+cents = .50
+
+print(dollars + cents)
+
+dollars = '3'
+
+print(dollars + "dollars")
+print(message)
+
+string_dollars = '3 dollars'
+string_cents = 'fifty cents'
+
+print(string_dollars+" "+string_cents)
+print(string_dollars, string_cents)
