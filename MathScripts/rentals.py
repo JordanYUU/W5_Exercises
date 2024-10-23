1ccd950bb5ff66819ef6e783ad2c7ade7c24529e
Jordan <jallen-chunn@appdev.yearup.org>
2024-10-23 13:09:28 -0500

    Updated rentals.py with clearer print statements

--- MathScripts/rentals.py
+++ MathScripts/rentals.py
@@ -7,4 +7,6 @@ vans_needed = round(tourists / van_limit + .5)
 rent_cost = van_cost * days_touring
 charge_per_person = round(rent_cost / tourists + .005, 2)
 
-print(vans_needed, rent_cost, charge_per_person)
+print('3 vans are needed for ' + str(tourists) + ' people')
+print('Rental cost is $' + str(rent_cost) + ' for 3 days')
+print('Charge is $' + str(charge_per_person) + ' per person')
